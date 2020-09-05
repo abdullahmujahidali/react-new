2bcb3c780a55a1cb75a2ae27db05fc66a407defa
Abdullah Mujahid <48271080+abdullahmujahidali@users.noreply.github.com>
2020-09-05 17:48:33 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite foods</h1>
+    <ul>
+      <li>Chinese</li>
+      <li>Chicken</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
